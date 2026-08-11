e59372e59e78c04fa67e52b71158778d196719ae
adomm16 <128422344+adomm16@users.noreply.github.com>
2026-08-11 13:30:39 +0300

    fix: use current UE build settings for editor target

--- game/AshfallProvinces/Source/AshfallProvincesEditor.Target.cs
+++ game/AshfallProvinces/Source/AshfallProvincesEditor.Target.cs
@@ -5,7 +5,7 @@ public class AshfallProvincesEditorTarget : TargetRules
     public AshfallProvincesEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("AshfallProvinces");
     }
